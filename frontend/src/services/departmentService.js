5dbc2b6d8f7bdbd48fc16f93f8333936325f91b6
Satyam Shukla <72057285+Satyam070@users.noreply.github.com>
2025-08-08 02:55:05 +0530

    Remove config.properties import for Railway deployment

--- frontend/src/services/departmentService.js
+++ frontend/src/services/departmentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8081/api/departments';
+const API_URL = `${process.env.REACT_APP_API_URL}/departments`;
 
 // Get all departments
 export const getAllDepartments = async () => {
--- frontend/src/services/employeeService.js
+++ frontend/src/services/employeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8081/api/employees';
+const API_URL = `${process.env.REACT_APP_API_URL}/employees`;
 
 // Get all employees
 export const getAllEmployees = async () => {
